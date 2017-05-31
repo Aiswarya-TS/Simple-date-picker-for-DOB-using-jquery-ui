8d0f9d3b624839874766056dfc6d26eae2aad3fa
Aiswarya T S <aiswarya439@gmail.com>
2017-05-31 11:28:09 +0530

    Add files via upload

---
+++ datepicker.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Date Picker</title>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $("#datepicker").datepicker({
+        changeMonth: true,
+        changeYear: true,
+  //yearRange: "1942:2001"
+});
+    
+  } );
+  </script>
+</head>
+
+<body>
+<input type="text" name="dob" id="datepicker" class="form-control" id="datepicker"  required="">
+</body>
+</html>
